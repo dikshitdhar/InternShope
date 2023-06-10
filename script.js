e108324bae666ed87164b21601466e6ec3cdc183
Dikshit Dhar <79019465+dikshitdhar@users.noreply.github.com>
2023-06-10 14:43:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var cart=0
+cons = addToCart = () => {
+    cart++
+    alert(" product added to cart ")
+    
+    
+}
+
+
+cons = checkout=()=> {
+    window.alert("order has been placed")
+    location.href='HTML.html'
+}
